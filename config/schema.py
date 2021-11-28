a55c391b37df20dc3e63eafe24619c2bf20dfadc
isdiscodead <isdiscodead@korea.ac.kr>
2021-11-28 20:04:59 +0900

    #4.4

--- config/schema.py
+++ config/schema.py
@@ -2,7 +2,13 @@ import graphene
 
 
 class Query(graphene.ObjectType):
-    pass
+    hello = graphene.String()   # string return query
+
+    # hello와 관련된 String resolver ( resolve_쿼리명() )
+    # info 객체는 resolver의 모든 정보( 서버, 연결, 요청 )를 가짐
+    def resolve_hello(self, info):
+        return "Hello"
+
 
 
 class Mutation():
