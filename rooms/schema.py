e0bd37511bdf99907bf3309347ebef68fca4e304
isdiscodead <isdiscodead@korea.ac.kr>
2021-12-03 23:42:39 +0900

    graphql pagination

--- rooms/schema.py
+++ rooms/schema.py
@@ -9,8 +9,18 @@ class RoomType(DjangoObjectType):
         model = Room
 
 
-class Query(object):
-    rooms = graphene.List(RoomType)
+class RoomListResponse(graphene.ObjectType):
+    list = graphene.List(RoomType)
+    total = graphene.Int()
 
-    def resolve_rooms(self, info):
-        pass
+
+class Query(object):
+    rooms = graphene.List(RoomListResponse, page=graphene.Int())
+
+    def resolve_rooms(self, info, page=1):
+        page_size = 5   # 한 페이지에 들어갈 개수
+        skipping = page_size * (page-1)
+        taking = page_size * page
+        rooms = Room.objects.all()[skipping:taking]
+        total = Room.objects.count()
+        return RoomListResponse(list=rooms, total=total)
