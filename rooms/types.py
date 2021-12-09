2d0fd619e8ebcef541dd39e2dc34f1ea94f625cd
isdiscodead <isdiscodead@korea.ac.kr>
2021-12-09 20:01:13 +0900

    dynamic field -> is_fav

--- rooms/types.py
+++ rooms/types.py
@@ -7,6 +7,13 @@ from rooms.models import Room
 class RoomType(DjangoObjectType):
 
     user = graphene.Field("users.schema.UserType")
+    is_fav = graphene.Boolean()
+
+    # resolve의 parent는 우리가 보고 있는 객체를 뜻함 -> room
+    def resolve_is_fav(room, info):
+        user = info.context.user
+        if user.is_authenticated:
+            return room in user.favsgi.all()
 
     class Meta:
         model = Room
